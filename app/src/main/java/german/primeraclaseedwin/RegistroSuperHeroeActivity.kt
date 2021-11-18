423f47d5ca51660fb5a08289f5a9c8d13793d371
GermanATA <ga.torres2283@gmail.com>
2021-11-17 21:39:12 -0500

    Parte tres, Registro de SuperHeroe 2

--- app/src/main/java/german/primeraclaseedwin/RegistroSuperHeroeActivity.kt
+++ app/src/main/java/german/primeraclaseedwin/RegistroSuperHeroeActivity.kt
@@ -1,7 +1,9 @@
 package german.primeraclaseedwin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.*
 import com.google.android.material.textfield.TextInputEditText
 
@@ -25,25 +27,39 @@ class RegistroSuperHeroeActivity : AppCompatActivity() {
         val ciudadSpiner: Spinner = findViewById(R.id.ciudad_spinner)
 
         registrarButton.setOnClickListener{
-            val nombre : String = nombreEditText.text.toString()
-            val estatura : Float = estaturaEditText.text.toString().toFloat()
-            val genero: String
-            var poderes = ""
-            val ciudad = ciudadSpiner.selectedItem.toString()
+            Log.d("Button","clicked")
 
-            if (masculinoRadioButton.isChecked)
-                genero = getString(R.string.masculino)
-            else
-                genero = getString(R.string.femenino)
+            //var dat : String ?// mi variable puede ser null y con la interrogacion no crashea cuando lo pongo
 
-            if (fuerzaCheckBox.isChecked)
-                poderes = getString(R.string.super_fuerza)
-            if (velocidadCheckBox.isChecked)
-                poderes = poderes + " " + getString(R.string.velocidad)
-            if (telequinesisCheckBox.isChecked)
-                poderes = poderes + " " + getString(R.string.telequinesis)
+            if (estaturaEditText.text.toString() == "")
+                estaturaEditText.error = "Digite una estatura Toche"
 
-            infoTextView.text = getString(R.string.nombre_para_registro,nombre,estatura,genero,poderes,ciudad)
+            if(nombreEditText.text.isEmpty() || estaturaEditText.text.toString() == "")
+                Toast.makeText(this,"Debe digitar el nombre y la estatura",Toast.LENGTH_SHORT).show()
+            else{
+                val nombre : String = nombreEditText.text.toString()
+                val estatura : Float = estaturaEditText.text.toString().toFloat()
+                val genero: String
+                var poderes = ""
+                val ciudad = ciudadSpiner.selectedItem.toString()
+
+                if (masculinoRadioButton.isChecked)
+                    genero = getString(R.string.masculino)
+                else
+                    genero = getString(R.string.femenino)
+
+                if (fuerzaCheckBox.isChecked)
+                    poderes = getString(R.string.super_fuerza)
+                if (velocidadCheckBox.isChecked)
+                    poderes = poderes + " " + getString(R.string.velocidad)
+                if (telequinesisCheckBox.isChecked)
+                    poderes = poderes + " " + getString(R.string.telequinesis)
+
+                infoTextView.text = getString(R.string.nombre_para_registro,nombre,estatura,genero,poderes,ciudad)
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
